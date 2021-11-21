5957601768cd095c94ba99284810db3981eeaf03
iamkdeepesh <69793773+iamkdeepesh@users.noreply.github.com>
2021-11-21 16:36:25 +0530

    Create NoOfDigits.java

---
+++ NoOfDigits.java
@@ -0,0 +1,14 @@
+package Assignment2ndWeek;
+
+public class NoOfDigits {
+public static void main(String cs[])
+{
+	int a=234567890, cout=0;
+	while(a>0)
+	{
+		cout++;
+		a=a/10;
+	}
+	System.out.println(cont);
+}
+}
